dd2aa5015ca97bca5ef035d5b2a9cbc35231ebdf
abhi5hek1979 <132596443+abhi5hek1979@users.noreply.github.com>
2025-11-21 13:37:59 +0530

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', function(e) {
-  self.skipWaiting();
-});
-
-self.addEventListener('fetch', function(event) {
-  // No-op, can be extended for offline features if needed
-});
